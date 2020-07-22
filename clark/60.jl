2d3002b81941ba66bbf3b3fd3a3b2c48786f8832
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-07-21 17:06:25 -0700

    starting in on 60 again

--- clark/60.jl
+++ clark/60.jl
@@ -30,6 +30,8 @@ build_pairs = function(upper = 10_000)
     # 2 cannot be in this set, since any number ending in 2 is not prime (besides 2).
     candidates = primes(3, upper)
 
+    # Why is this Dict{Any,Any}? 
+    # The keys are integers and the values are sets of integers
     d = Dict()
     for (i, p) in Iterators.enumerate(candidates)
         larger_candidates = candidates[(i+1):end]
@@ -58,8 +60,41 @@ end
 
 # All those that have fewer than 4 elements cannot be in this set.
 # We could keep filtering them down, but we'll still need some brute force in the end.
-# May as well write the brute force now, 
+# May as well write the brute force now
 
 # Suppose we represent the graph with a (sparse) matrix.
 # To find the fully connected subcomponents, we just need to get the graph into a form with diagonal blocks:
 # AD, where A is a permutation matrix.
+
+
+# Tue Jul 21 16:46:11 PDT 2020
+#
+# Starting again.
+# We're not looking for just any 5 clique, we're looking for the 5 clique with the lowest sum.
+# We can use this property to improve how we search.
+# Start with the smallest numbers in the list, and gradually add larger numbers.
+# The idea is to iterate through the 
+
+
+"""
+Are nodes a and b connected, as represented by d?
+"""
+connected = function(a, b, d)
+    if b < a
+        a, b = b, a
+    end
+    b in d[a]
+end
+
+
+"""
+Find a k clique.
+It doesn't need to be the smallest one.
+Mutate d by removing all nodes that cannot be in a k clique.
+"""
+find_k_clique = function(d, k = 5)
+    for n, edges in d
+        if length(edges) < k
+
+    end
+end
